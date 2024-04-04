fa1240af54506a3886e357d7cd7c0b6a56b5f9a4
kenzalmario@gmail.com <kenzalmario@gmail.com>
2024-04-05 00:48:28 +0800

    update of transaction by date

--- WalletService.Application/Requests/Accounts/Queries/GetPagedAccountTransactions/GetPagedAccountTransactionsQuery.cs
+++ WalletService.Application/Requests/Accounts/Queries/GetPagedAccountTransactions/GetPagedAccountTransactionsQuery.cs
@@ -1,6 +1,4 @@
 using MediatR;
-using Microsoft.EntityFrameworkCore;
-using WalletService.Application.Interfaces;
 using WalletService.Domain.Enums;
 
 namespace WalletService.Application.Requests.Accounts.Queries.GetAccountTransactions;
@@ -14,64 +12,4 @@ public class GetPagedAccountTransactionsQuery : IRequest<AccountDto>
     public int PageSize { get; set; } = 20;
     public DateTime? StartDate { get; set; } = DateTime.Now.AddDays(-7);
     public DateTime? EndDate { get; set; } = DateTime.Now;
-}
-
-public class GetPagedAccountTransactionsQueryHandler : IRequestHandler<GetPagedAccountTransactionsQuery, AccountDto>
-{
-    private readonly IWalletDbContext _walletDbContext;
-
-    public GetPagedAccountTransactionsQueryHandler(IWalletDbContext walletDbContext)
-    {
-        _walletDbContext = walletDbContext;
-    }
-
-    public async Task<AccountDto> Handle(GetPagedAccountTransactionsQuery request, CancellationToken cancellationToken)
-    {
-        int totalCount = 0;
-
-        var accountDoc = await _walletDbContext.AccountWallets.Where(o => o.Id == request.AccountId)
-            .FirstOrDefaultAsync(cancellationToken);
-
-        var accountTransactions = accountDoc.Account.WalletTransactions;
-
-        if (!string.IsNullOrEmpty(request.SearchKey))
-            accountTransactions = accountTransactions.Where(t => t.TransactionReference.Contains(request.SearchKey)
-                || t.TransactionNo.Contains(request.SearchKey)
-                || t.ModeOfTransaction.Contains(request.SearchKey)
-                || t.Notes.Contains(request.SearchKey)).ToList();
-
-        if (request.TransactionType.HasValue)
-            accountTransactions = accountTransactions.Where(t => t.TransactionType == request.TransactionType).ToList();
-
-        if (request.StartDate.HasValue)
-            accountTransactions = accountTransactions.Where(t => t.TransactionDate >= request.StartDate).ToList();
-
-        if (request.EndDate.HasValue)
-            accountTransactions = accountTransactions.Where(t => t.TransactionDate <= request.EndDate).ToList();
-
-        totalCount = accountTransactions.Count();
-
-        var transactions = accountTransactions.Select(o => new AccountTransactionDto
-        {
-            Id = o.Id,
-            TransactionNo = o.TransactionNo,
-            TransactionType = o.TransactionType,
-            TransactionReference = o.TransactionReference,
-            Amount = o.Amount,
-            TransactionDate = o.TransactionDate,
-            Credit = o.Credit,
-            ModeOfTransaction = o.ModeOfTransaction,
-            Notes = o.Notes
-        })
-        .Skip(request.Start)
-        .Take(request.PageSize);
-
-        return new AccountDto(transactions)
-        {
-            AccountId = accountDoc.Id,
-            AccountType = accountDoc.Account.AccountType,
-            Offset = request.Start + request.PageSize + 1,
-            TotalCount = totalCount
-        };
-    }
 }
